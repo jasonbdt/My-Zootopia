daba2f1ea988229c7fab25965ca8147832efbfbb
jasonbdt <ja.bladt@gmail.com>
2025-09-22 01:05:19 +0200

    add possibility to filter animals by skin type

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,13 +13,15 @@ def load_data(file_path: str):
         print(f"File {file_path} doesn't exist!")
 
 
-def serialize_animal(animal_obj: dict[str, Any]) -> str:
+def serialize_animal(animal_obj: dict[str, Any]) -> str | None:
     output = ''
     try:
         name, locations = animal_obj['name'], animal_obj['locations']
-        diet, lifespan, skin_type = (animal_obj['characteristics'].get('diet', 'N/A'),
-                                     animal_obj['characteristics'].get('lifespan', 'N/A'),
-                                     animal_obj['characteristics'].get('skin_type', 'N/A'))
+        diet, lifespan, skin_type = (
+            animal_obj['characteristics'].get('diet', 'N/A'),
+            animal_obj['characteristics'].get('lifespan', 'N/A'),
+            animal_obj['characteristics'].get('skin_type', 'N/A')
+        )
     except KeyError:
         print("Fatal error occurred, unable to serialize animal!")
     else:
@@ -39,16 +41,48 @@ def serialize_animal(animal_obj: dict[str, Any]) -> str:
         output += "</ul></div></li>\n"
 
         return output
+    return None
 
-    return ""
+
+def get_unique_skin_types(animals) -> set[str]:
+    unique_skin_types = {
+        animal_obj['characteristics']['skin_type'] for animal_obj in animals
+        if 'skin_type' in animal_obj['characteristics']
+    }
+    unique_skin_types.add('N/A')
+    return unique_skin_types
+
+
+def filter_by_skin_type(animal_obj, chosen_skin_type: str) -> bool:
+    if chosen_skin_type == "":
+        return True
+    elif chosen_skin_type == "N/A":
+        if 'skin_type' not in animal_obj['characteristics']:
+            return True
+
+    if 'skin_type' in animal_obj['characteristics']:
+        animal_skin_type = animal_obj['characteristics']['skin_type']
+        if animal_skin_type.lower() == chosen_skin_type.lower():
+            return True
+
+    return False
 
 
 def main() -> None:
     animals_data = load_data('animals_data.json')
     html_content = load_data('animals_template.html')
 
+    skin_types = sorted(get_unique_skin_types(animals_data))
+    print(f"Available skin types: {", ".join(skin_types)}\n")
+
+    user_choice = input("Please choose a skin type (leave blank for no filter): ")
+    filtered_animals = filter(
+        lambda animal_obj: filter_by_skin_type(animal_obj, user_choice),
+        animals_data
+    )
+
     output = ""
-    for animal_obj in animals_data:
+    for animal_obj in filtered_animals:
         output += serialize_animal(animal_obj)
 
     if html_content:
